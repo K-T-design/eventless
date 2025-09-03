677182c9533e1aaf7e483beabbf16ee8f7460a8e
CHIEMELIE NWAMARA <kingtitanium15@gmail.com>
2025-09-03 20:06:09 +0000

    This error is a React hydration mismatch, which means the server-rendere

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className="font-body antialiased flex flex-col min-h-screen">
+      <body className="font-body antialiased flex flex-col min-h-screen" suppressHydrationWarning={true}>
         <Header />
         <main className="flex-grow">{children}</main>
         <Footer />
